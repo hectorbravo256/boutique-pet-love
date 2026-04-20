ef7b7c4896932471509a2765beecf99eaefa82e7
Hector Bravo <hectorbravo256@gmail.com>
2026-04-20 18:27:12 -0400

    fix functions

--- netlify/functions/get-orders.js
+++ netlify/functions/get-orders.js
@@ -1,25 +1,6 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
-
 exports.handler = async () => {
-  const { data, error } = await supabase
-    .from("orders")
-    .select("*")
-    .order("created_at", { ascending: false });
-
-  if (error) {
-    return {
-      statusCode: 500,
-      body: error.message,
-    };
-  }
-
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify([{ test: "funciona" }]),
   };
 };
