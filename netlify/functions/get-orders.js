4a721878c53a16a3cdc81e4d6df280e83510c8fe
hectorbravo256 <hectorbravo256@gmail.com>
2026-05-06 17:55:36 -0400

    Update get-orders.js

--- netlify/functions/get-orders.js
+++ netlify/functions/get-orders.js
@@ -2,7 +2,16 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+    realtime: {
+      enabled: false,
+    },
+  }
 );
 
 exports.handler = async () => {
