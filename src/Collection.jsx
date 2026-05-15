6bfbef1c6f2b5f75a4aca9fbfeee3a47114c0b9a
hectorbravo256 <hectorbravo256@gmail.com>
2026-05-15 17:07:57 -0400

    Update Collection.jsx

--- src/Collection.jsx
+++ src/Collection.jsx
@@ -75,6 +75,17 @@ export default function Collection() {
     const { data } =
       await query;
 
+    (data || []).forEach(product => {
+
+  product.product_images?.sort(
+    (a, b) =>
+      (a.sort_order || 0)
+      -
+      (b.sort_order || 0)
+  );
+
+});
+
     setProducts(data || []);
 
     setLoading(false);
