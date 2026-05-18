83e775a543b88134a591fa468373cca214d6cb39
hectorbravo256 <hectorbravo256@gmail.com>
2026-05-18 19:06:16 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import {
+  ToastProvider
+} from "./components/ToastProvider";
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+
   <React.StrictMode>
-    <App />
+
+    <ToastProvider>
+
+      <App />
+
+    </ToastProvider>
+
   </React.StrictMode>
+
 );
