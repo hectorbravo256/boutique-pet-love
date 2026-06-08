754e996d61a4d7fbc3dfb8becaf0ea02c90be912
hectorbravo256 <hectorbravo256@gmail.com>
2026-06-08 12:16:17 -0400

    Update Gender.jsx

--- src/Gender.jsx
+++ src/Gender.jsx
@@ -150,7 +150,7 @@ export default function Gender() {
 
               onClick={() =>
                 navigate(
-                  `/producto/${product.id}`
+                  `/producto/${product.slug}`
                 )
               }
 
