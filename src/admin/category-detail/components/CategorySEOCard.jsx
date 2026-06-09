d164ed0b4033718c1df2c72e9170cb70ca6a802b
hectorbravo256 <hectorbravo256@gmail.com>
2026-06-09 11:39:29 -0400

    Update CategorySEOCard.jsx

--- src/admin/category-detail/components/CategorySEOCard.jsx
+++ src/admin/category-detail/components/CategorySEOCard.jsx
@@ -51,6 +51,15 @@ export default function CategorySEOCard({
           "
         />
 
+        <div className="
+  text-xs
+  text-slate-500
+  text-right
+">
+  {(category.seo_title || "").length}
+  / 60 caracteres
+</div>
+
         <textarea
 
           rows={3}
@@ -78,6 +87,15 @@ export default function CategorySEOCard({
             py-4
           "
         />
+
+        <div className="
+  text-xs
+  text-slate-500
+  text-right
+">
+  {(category.seo_description || "").length}
+  / 160 caracteres
+</div>
 
         <textarea
 
@@ -108,6 +126,56 @@ export default function CategorySEOCard({
         />
 
       </div>
+
+      {/* PREVIEW GOOGLE */}
+
+<div className="
+  mt-8
+
+  border
+  border-slate-200
+
+  rounded-3xl
+
+  p-6
+
+  bg-slate-50
+">
+
+  <div className="
+    text-xs
+    text-green-700
+    mb-2
+  ">
+    boutiquepetlove.cl/categoria/{category.slug}
+  </div>
+
+  <div className="
+    text-xl
+    text-blue-700
+
+    font-medium
+
+    mb-2
+  ">
+    {
+      category.seo_title ||
+      "Título SEO"
+    }
+  </div>
+
+  <div className="
+    text-sm
+    text-slate-600
+    leading-6
+  ">
+    {
+      category.seo_description ||
+      "Descripción SEO"
+    }
+  </div>
+
+</div>
 
     </AdminCard>
 
