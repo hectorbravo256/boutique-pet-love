bcfdc5b510c82c0d60ed0439f3522a076410b813
hectorbravo256 <hectorbravo256@gmail.com>
2026-06-09 10:29:11 -0400

    Create CategoriaDetalle.jsx

---
+++ src/admin/CategoriaDetalle.jsx
@@ -0,0 +1,20 @@
+export default function CategoriaDetalle() {
+
+  return (
+
+    <div className="
+      p-8
+    ">
+
+      <h1 className="
+        text-4xl
+        font-black
+      ">
+        CategoriaDetalle
+      </h1>
+
+    </div>
+
+  );
+
+}
