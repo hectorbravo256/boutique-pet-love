8946bc5a35f5a2e639a776b4fffa33c78c36bf41
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-03 18:13:57 -0400

    Update PurchaseForm.jsx

--- src/admin/inventory/PurchaseForm.jsx
+++ src/admin/inventory/PurchaseForm.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../../supabaseClient";
 import AdminCard from "../components/AdminCard";
 
 export default function PurchaseForm() {
+
+    const [products, setProducts] = useState([]);
+
+const [variants, setVariants] = useState([]);
+
+const [detail, setDetail] = useState({
+    product_id: "",
+    variant_id: "",
+    quantity: 1,
+    unit_cost: 0
+});
+
+const [details, setDetails] = useState([]);
+
+    useEffect(() => {
+
+    cargarProductos();
+
+}, []);
+
+const cargarProductos = async () => {
+
+    const { data } =
+        await supabase
+            .from("products")
+            .select("id,name")
+            .eq("active", true)
+            .order("name");
+
+    setProducts(data || []);
+
+};
 
     return (
 
@@ -47,6 +81,34 @@ export default function PurchaseForm() {
             </div>
 
         </AdminCard>
+
+        <select
+    value={detail.product_id}
+    onChange={(e)=>
+        setDetail({
+            ...detail,
+            product_id:e.target.value
+        })
+    }
+    className="border rounded-xl p-3"
+>
+
+<option value="">
+    Seleccionar producto
+</option>
+
+{
+products.map(product=>(
+<option
+    key={product.id}
+    value={product.id}
+>
+    {product.name}
+</option>
+))
+}
+
+</select>
 
     );
 
