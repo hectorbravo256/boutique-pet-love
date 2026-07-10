c8c0b5c0578d734c790371c44435814c782d7f0d
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-10 17:26:16 -0400

    Update PurchaseFooter.jsx

--- src/admin/inventory/components/PurchaseFooter.jsx
+++ src/admin/inventory/components/PurchaseFooter.jsx
@@ -1,1 +1,42 @@
+import AdminCard from "../../components/AdminCard";
 
+export default function PurchaseFooter() {
+
+    return (
+
+        <AdminCard>
+
+            <div className="flex justify-end gap-4">
+
+                <button
+                    className="
+                        px-6
+                        py-3
+                        rounded-xl
+                        bg-slate-200
+                        font-bold
+                    "
+                >
+                    Cancelar
+                </button>
+
+                <button
+                    className="
+                        px-6
+                        py-3
+                        rounded-xl
+                        bg-pink-500
+                        text-white
+                        font-bold
+                    "
+                >
+                    Guardar compra
+                </button>
+
+            </div>
+
+        </AdminCard>
+
+    );
+
+}
