9f3d75cb162f8a69161b91e0a7e26b2ca6353f8d
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-17 20:46:37 -0400

    Update purchaseService.js

--- src/admin/inventory/services/purchaseService.js
+++ src/admin/inventory/services/purchaseService.js
@@ -1,1 +1,41 @@
+import { supabase } from "../../../supabaseClient";
 
+class PurchaseService {
+
+    //----------------------------------------
+    // Crear cabecera compra
+    //----------------------------------------
+
+    async createPurchase({
+
+        supplier,
+
+        invoiceNumber,
+
+        observations
+
+    }) {
+
+        const { data, error } =
+            await supabase.rpc(
+                "registrar_compra",
+                {
+
+                    p_supplier: supplier,
+
+                    p_invoice: invoiceNumber,
+
+                    p_observations: observations
+
+                }
+            );
+
+        if (error) throw error;
+
+        return data;
+
+    }
+
+}
+
+export default new PurchaseService();
