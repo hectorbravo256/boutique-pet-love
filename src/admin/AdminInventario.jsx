b128b688331c1db68ef431a4667fa51cdf7ce957
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-18 02:07:30 -0400

    Update AdminInventario.jsx

--- src/admin/AdminInventario.jsx
+++ src/admin/AdminInventario.jsx
@@ -8,28 +8,7 @@ export default function AdminInventario() {
 
     <div className="max-w-[1500px] mx-auto p-8">
 
-      <div className="mb-10">
 
-        <p className="
-          uppercase
-          tracking-[0.3em]
-          text-pink-500
-          text-xs
-          font-bold
-        ">
-          Administración
-        </p>
-
-        <h1 className="
-          text-5xl
-          font-black
-          text-slate-900
-          mt-3
-        ">
-          📦 Inventario
-        </h1>
-
-      </div>
 
 <InventoryDashboard />
 
