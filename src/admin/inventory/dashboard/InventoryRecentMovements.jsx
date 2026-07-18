088bddfe98f317bfe671b34051989f35b5607aba
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-18 02:37:37 -0400

    Create InventoryRecentMovements.jsx

---
+++ src/admin/inventory/dashboard/InventoryRecentMovements.jsx
@@ -0,0 +1,14 @@
+import InventoryMovements from "../InventoryMovements";
+import SectionTitle from "../../shared/ui/SectionTitle";
+
+export default function InventoryRecentMovements() {
+  return (
+    <>
+      <SectionTitle>
+        Últimos movimientos
+      </SectionTitle>
+
+      <InventoryMovements />
+    </>
+  );
+}
