5861abe8cab9e9f260f09111cd0a3458a542f6e4
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-18 01:55:08 -0400

    Update AdminLayout.jsx

--- src/admin/shared/layout/AdminLayout.jsx
+++ src/admin/shared/layout/AdminLayout.jsx
@@ -1,1 +1,27 @@
+import Sidebar from "./Sidebar";
+import TopBar from "./TopBar";
+import Content from "./Content";
 
+export default function AdminLayout({
+  children,
+}) {
+  return (
+    <div className="flex h-screen bg-slate-100">
+
+      <Sidebar />
+
+      <div className="flex flex-col flex-1">
+
+        <TopBar />
+
+        <Content>
+
+          {children}
+
+        </Content>
+
+      </div>
+
+    </div>
+  );
+}
