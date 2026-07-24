4dfeea4d190af68190a73916c2a526b086b1559e
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-24 15:14:39 -0400

    Create DashboardService.js

---
+++ src/admin/shared/services/DashboardService.js
@@ -0,0 +1,14 @@
+import ApiClient from "../api/ApiClient";
+
+class DashboardService {
+
+    async getSummary() {
+
+        // Aquí iremos agregando las consultas del dashboard
+        return {};
+
+    }
+
+}
+
+export default new DashboardService();
