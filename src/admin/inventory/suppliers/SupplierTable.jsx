a9e5b7c9e775fa2bf501155ba4fe1e82015a570e
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-27 17:17:45 -0400

    Update SupplierTable.jsx

--- src/admin/inventory/suppliers/SupplierTable.jsx
+++ src/admin/inventory/suppliers/SupplierTable.jsx
@@ -1,10 +1,81 @@
+import useSuppliers from "./useSuppliers";
+
 export default function SupplierTable() {
+
+    const {
+        suppliers,
+        loading
+    } = useSuppliers();
+
+    if (loading) {
+
+        return <p>Cargando proveedores...</p>;
+
+    }
 
     return (
 
-        <div className="mt-8 rounded-3xl bg-white p-8 shadow">
+        <div className="mt-8 rounded-3xl bg-white shadow overflow-hidden">
 
-            Tabla de proveedores
+            <table className="w-full">
+
+                <thead>
+
+                    <tr>
+
+                        <th className="p-4 text-left">Proveedor</th>
+
+                        <th>Contacto</th>
+
+                        <th>Teléfono</th>
+
+                        <th>Email</th>
+
+                        <th>Estado</th>
+
+                    </tr>
+
+                </thead>
+
+                <tbody>
+
+                    {suppliers.map(supplier => (
+
+                        <tr key={supplier.id}>
+
+                            <td className="p-4">
+
+                                {supplier.name}
+
+                            </td>
+
+                            <td>{supplier.contact_name}</td>
+
+                            <td>{supplier.phone}</td>
+
+                            <td>{supplier.email}</td>
+
+                            <td>
+
+                                {
+
+                                    supplier.active
+
+                                        ? "🟢 Activo"
+
+                                        : "🔴 Inactivo"
+
+                                }
+
+                            </td>
+
+                        </tr>
+
+                    ))}
+
+                </tbody>
+
+            </table>
 
         </div>
 
