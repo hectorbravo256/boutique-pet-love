2a0e26f3ada71f71be808325666684dc0f17d875
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-27 17:15:59 -0400

    Create supplierService.js

---
+++ src/admin/inventory/suppliers/supplierService.js
@@ -0,0 +1,56 @@
+import { supabase } from "../../../supabaseClient";
+
+export async function getSuppliers() {
+
+    const { data, error } = await supabase
+        .from("suppliers")
+        .select("*")
+        .order("name");
+
+    if (error) throw error;
+
+    return data;
+
+}
+
+export async function createSupplier(values) {
+
+    const { data, error } = await supabase
+        .from("suppliers")
+        .insert(values)
+        .select()
+        .single();
+
+    if (error) throw error;
+
+    return data;
+
+}
+
+export async function updateSupplier(id, values) {
+
+    const { data, error } = await supabase
+        .from("suppliers")
+        .update(values)
+        .eq("id", id)
+        .select()
+        .single();
+
+    if (error) throw error;
+
+    return data;
+
+}
+
+export async function deleteSupplier(id) {
+
+    const { error } = await supabase
+        .from("suppliers")
+        .update({
+            active: false
+        })
+        .eq("id", id);
+
+    if (error) throw error;
+
+}
