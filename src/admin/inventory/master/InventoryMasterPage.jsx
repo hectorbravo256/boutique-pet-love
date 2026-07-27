b5d4667b8d0337c36977756f9c51bd7f19b268ad
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-27 01:19:39 -0400

    Update InventoryMasterPage.jsx

--- src/admin/inventory/master/InventoryMasterPage.jsx
+++ src/admin/inventory/master/InventoryMasterPage.jsx
@@ -1,10 +1,30 @@
+import { Link } from "react-router-dom";
 import InventoryMasterTable from "../InventoryMasterTable";
 import Button from "../../shared/ui/Button";
 import PageHeader from "../../shared/ui/PageHeader";
 
+
 export default function InventoryMasterPage() {
     return (
         <div className="max-w-[1500px] mx-auto p-8">
+
+            <div className="mb-5">
+
+    <Link
+        to="/admin/inventario"
+        className="
+            inline-flex
+            items-center
+            gap-2
+            text-pink-600
+            font-semibold
+            hover:underline
+        "
+    >
+        ← Volver a Inventario
+    </Link>
+
+</div>
 
             <PageHeader
                 icon="📦"
