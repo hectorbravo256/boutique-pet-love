0c9633875fff11ea71b77afd232a6deffd531054
hectorbravo256 <hectorbravo256@gmail.com>
2026-07-28 17:46:29 -0400

    Update PurchaseProductModal.jsx

--- src/admin/inventory/purchases/components/PurchaseProductModal.jsx
+++ src/admin/inventory/purchases/components/PurchaseProductModal.jsx
@@ -1,1 +1,39 @@
+import Modal from "../../shared/ui/Modal";
 
+import PurchaseProductSelector from "./PurchaseProductSelector";
+
+export default function PurchaseProductModal({
+
+    open,
+
+    onClose,
+
+    ...props
+
+}) {
+
+    if (!open) return null;
+
+    return (
+
+        <Modal
+
+            title="Agregar producto"
+
+            open={open}
+
+            onClose={onClose}
+
+        >
+
+            <PurchaseProductSelector
+
+                {...props}
+
+            />
+
+        </Modal>
+
+    );
+
+}
