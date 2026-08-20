0388d7493c193311265192b9991f9c625d0dc755
hectorbravo256 <hectorbravo256@gmail.com>
2026-08-19 23:47:17 -0400

    Update VentasExternas.jsx

--- src/admin/inventory/VentasExternas.jsx
+++ src/admin/inventory/VentasExternas.jsx
@@ -414,14 +414,14 @@ export default function VentasExternas() {
 
         try {
 
-            const payloadItems =
-                items.map(item => ({
-                    variant_id:
-                        Number(item.variant_id),
+const payloadItems =
+    items.map(item => ({
+        variant_id:
+            Number(item.variant_id),
 
-                    quantity:
-                        Number(item.quantity)
-                }));
+        qty:
+            Number(item.quantity)
+    }));
 
 
             const {
